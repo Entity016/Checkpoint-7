6961bf4d9531a342db41c1a519766ce8fd1857ba
Pablo <pablo3011ga@gmail.com>
2025-05-09 21:55:03 +0200

    Added html, js and images
    Added first version of the answers html, javascript exercise and the images

---
+++ checkpoint7.js
@@ -0,0 +1,12 @@
+function cuatroarg( a, b, c, d){
+    const suma_1 = a + b;
+    const suma_2 = c + d;
+    const suma_total = suma_1 * suma_2;
+  
+  
+  if (suma_total > 50){
+    console.log ("EL resultado es mayor que 50");
+  } else {
+    console.log ("EL resultado es menos que 50")
+  }
+}
